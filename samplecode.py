b6b01e71ff7df8a70729511bd90ada67b5b957d5
brina-cloud <s.kaliza1@alustudent.com>
2026-01-28 14:10:32 +0200

    Implement security function to check for risky patterns in input

--- samplecode.py
+++ samplecode.py
@@ -10,11 +10,24 @@ with open('sampleinput.txt', 'r') as f:
 """ Risky patterns to look for """
 wrong_pattern = ['<script>', '</script>', 'javascript:', 'onload=', 'onclick=', 'onerror=', 'select *', 'insert into', 'delete from', 'update set', 'drop']
 
-phone_number_pattern = r'
-email_address_pattern = r'
+
+phone_number_pattern = 
+email_address_pattern = 
 hash_tag_pattern = 
 time_pattern = 
 
+def security(word):
+    """ Check if a word contains any risky patterns and return false """
+    word_lower = word.lower()
+    for pattern in wrong_pattern:
+        if pattern in word_lower:
+            return False
+    return True
 
 
 
+    
+
+
+
+    
